70e7e2e6fa76f20bf72519bedec5c0ec419813d2
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-08-08 22:55:22 +0530

    Add files via upload

---
+++ Array9.java
@@ -0,0 +1,11 @@
+ class Array9 {
+    public static void main(String [] args)
+    {
+        int [][]arr=new int[3][4];
+        System.out.println("Total no of row "+arr.length);
+        System.out.println("total No of columna "+arr[0].length);
+
+        }
+    
+    
+}
