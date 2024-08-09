c570894bc49327485a93f2b02bb87ce24b63d9e8
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-08-09 13:57:05 +0530

    Add files via upload

---
+++ Array11.java
@@ -0,0 +1,24 @@
+public class Array11 
+{
+    public static void main(String [] args)
+    {
+        int arr[][]={{1,2,3},
+                     {4,5,6},
+                     {7,8,9}};
+        int ar1[][]={{10,20,30},
+                     {10,20,30},
+                     {10,20,30}};
+        int arr2[][]=new int[3][3];
+        for(int i=0;i<arr.length;i++)
+
+        { 
+            for(int j=0;j<arr.length;j++)
+            {
+                
+                arr2[i][j]=ar1[i][j]+arr[i][j];
+                System.out.print(arr2[i][j]+" ");
+                //System.out.print(arr2[j]);
+            }
+        }
+    }
+}
