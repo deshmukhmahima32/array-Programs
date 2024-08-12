d0fe6245345ccb96868a691435ff160d4d803c95
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-08-12 22:47:11 +0530

    Add files via upload

---
+++ Arrayalternate.java
@@ -0,0 +1,13 @@
+public class Arrayalternate{
+    public static void main(String [] args)
+      {
+        int arr[]={1,2,3,5,6,8,9};
+        for(int i=0;i<arr.length;i=i+2)
+        {
+               System.out.println(arr[i]);
+               
+        }
+      }
+}
+    
+
