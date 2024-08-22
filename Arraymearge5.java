fde1fbb7f9e12b0c92f340719e5ad447ba2736f8
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-08-22 22:59:12 +0530

    Add files via upload

---
+++ Arraymearge5.java
@@ -0,0 +1,15 @@
+public class Arraymearge5 {
+    public static void main(String[] args) {
+        int arr1[]={1,2,3};
+        int arr2[]={5,6,7};
+        int newarry[]=new int[6];
+        int x=0;
+        for(int i=0;i<arr1.length;i++);
+        {
+            newarry[x]=arr1[];
+
+        }
+    }
+    
+}
+ 
